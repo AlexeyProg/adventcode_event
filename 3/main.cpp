0251165321b3bb39c3d90ab3bd804eefb93d1cbb
AlexeyDeveloper <sokolov-lesha@yandex.ru>
2023-12-16 17:21:31 +0300

    third +

--- 3/main.cpp
+++ 3/main.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <unordered_map>
 
+#define IS_SYMB(x) (x != '.' && !isdigit(x))
+
+
 using namespace std;
 
 vector<string>file_str;
@@ -25,27 +28,47 @@ void read_file()
 class First
 {
 public:
+
+    vector<int> digits;
+
     void solution()                     // . . . 4
     {                                   // 3 * . 1
                                         // 3 * . .
-        vector<int> digits;
 
         for(int i = 0; i < file_str.size(); i++)
         {
             string dig;
+            bool flag = false;
             for(int j = 0; j < file_str[i].size(); j++)
             {
-                char cur_s = file_str[i][j];
+                char cur_s = file_str[i][j];            // \* * *
+                                                        // \* 2 *
+                                                        // \* * *
                 
                 if(isdigit(cur_s))
                 {
-                    dig += cur_s;    
+                    dig += cur_s;
+                    if((j > 0 && IS_SYMB(file_str[i][j-1]))  || (j < file_str.size()-1 && IS_SYMB(file_str[i][j+1])) 
+                    || ( i > 0 && IS_SYMB(file_str[i-1][j])) ||  (j > 0 && i < file_str[i].size()-1 && IS_SYMB(file_str[i+1][j-1])) 
+                     || (i < file_str.size()-1 && j > 0 && IS_SYMB(file_str[i+1][j-1])) || 
+                    (i < file_str.size()-1 && IS_SYMB(file_str[i+1][j])) || (i > 0 && j > 0 && IS_SYMB(file_str[i-1][j-1])) || 
+                    (i > 0 && j < file_str[i].size()-1 && IS_SYMB(file_str[i-1][j+1])) || (i < file_str.size()-1 && j < file_str[i].size() && IS_SYMB(file_str[i+1][j+1])) )
+                    {
+                        flag = true;
+                    }
                 }
-                else
+                else 
                 {
-                    int x = stoi(dig);
-                    digits.push_back(x);
-                    dig.clear();
+                    if(!dig.empty() && flag)
+                    {
+                        // cout << dig << endl;
+                        int x = stoi(dig);
+                        digits.push_back(x);
+                        dig.clear();
+                        flag = false;
+                    }
+                    else
+                        dig.clear();
                 }
             }
         }
@@ -61,5 +84,11 @@ int main()
     // {
     //     cout << file_str[i] << " - ";
     // }
+    First *f = new First();
+    f->solution();
+
+    for(const auto &x : f->digits)
+        cout << x << " " ;
+
     return 0;
 }
